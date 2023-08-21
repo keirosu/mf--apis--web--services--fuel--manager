26dbf7e7e2805205532fc41e83319cf50486d393
keirosu <103314640+keirosu@users.noreply.github.com>
2023-08-21 11:31:06 +0900

    Criação e Configuração dos modelos de Veículo e Consumo

---
+++ mf- apis- web- services- fuel- manager/Consumo.cs
@@ -0,0 +1,42 @@
+﻿using mf__apis__web__services__fuel__manager.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf__apis__web__services__fuel__manager
+{
+    [Table("Consumos")]
+
+    public class Consumo
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+
+        public string Descricao { get; set; }
+        [Required]
+
+        public DateTime Data { get; set; }
+        [Required]
+
+        public decimal Valor { get; set; }
+        [Required]
+
+        public TipoCombustivel Tipo { get; set; }
+        
+        
+        [Required]
+
+        public int VeiculoId { get; set; }
+
+
+        public Veiculo Veiculo { get; set; }
+    }
+
+    public enum TipoCombustivel 
+    { 
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
---
+++ mf- apis- web- services- fuel- manager/Models/Veiculo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf__apis__web__services__fuel__manager.Models
+{
+    [Table("Veiculos")]
+    
+    public class Veiculo
+    {
+        [Key]
+
+        public int id { get; set; }
+        [Required]
+
+        public string Marca { get; set; }
+        [Required]
+
+        public string Modelo { get; set; }
+        [Required]
+
+        public string Placa { get; set; }
+        [Required]
+
+        public int AnoFabricacao { get; set; }
+        [Required]
+
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+
+    }
+}
